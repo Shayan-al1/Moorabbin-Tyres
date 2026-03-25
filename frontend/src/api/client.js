5eda2be2adde723a5a096ebbafb3636f8fe19c70
ShayanGlacier <shayan@glacieragency.com>
2026-03-25 21:11:10 +0500

    fix bc bc mc

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Change this value when the backend base URL moves.
-export const API_BASE_URL = "api.moorabbintyres.com/api";
+export const API_BASE_URL = "https://api.moorabbintyres.com/api";
 
 const client = axios.create({
   baseURL: API_BASE_URL,
